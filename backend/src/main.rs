7385c72cdd5618a22e8241976117b61d8efb90c4
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-10-14 04:05:48 -0400

    Commiting what I have for now, almost finished but not done

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,7 +7,7 @@ use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::post;
 use tower_http::cors::{Any, CorsLayer};
-use crate::types::{OptimizePrompt, ItemsPrompt, gptcall, shoppingapicall};
+use crate::types::{OptimizePrompt, ItemsPrompt, gptcall, shoppingapicall, OptimizedItem};
 use reqwest::Error;
 use futures::future::join_all;
 
@@ -45,7 +45,18 @@ async fn items_gen(Json(request_data): Json<ItemsPrompt>) -> impl IntoResponse {
 }
 
 async fn optimize_items(Json(request_data): Json<OptimizePrompt>) -> impl IntoResponse {
-    let futures = join_all(request_data.items.iter().map(|item| async { shoppingapicall(item.to_string()).await.unwrap() })).await;
-
-    (StatusCode::OK, Json(futures))
+    let items_costs = join_all(request_data.items.iter().map(|item| async { shoppingapicall(item.to_string()).await.unwrap() })).await;
+    let formatted_items = format!("{:?}", request_data.items);
+    let formatted_costs = format!("{:?}", items_costs);
+    (StatusCode::OK, Json(items_costs))
+    // println!("Done grabbing shit");
+    // let prompt = format!("Given items \n {} \n and given multiple links and costs for ever item in the list above \n {} \n\
+    // for every item, return the item name from the original list, the cost and link to the option you found. The total cost for everything must fit inside budget of {} dollars and you should give me a list"
+    //     , formatted_items, formatted_costs, request_data.budget);
+    // let response = gptcall(prompt.to_string()).await;
+    // (StatusCode::OK, Json(response.unwrap()))
 }
+
+// async fn price_cleanup(response: Result<String, Error>) -> Vec<OptimizedItem>{
+//
+// }
--- backend/src/types.rs
+++ backend/src/types.rs
@@ -2,6 +2,8 @@ use reqwest::Error;
 use reqwest::header::{HeaderMap, HeaderValue};
 use serde::{Deserialize, Serialize};
 use crate::keys;
+use std::any::type_name;
+use serde_json::from_value;
 
 #[derive(Serialize, Deserialize)]
 pub struct ItemsPrompt {
@@ -24,6 +26,13 @@ pub struct GptData {
 pub struct GptMessage {
     pub(crate) role: String,
     pub(crate) content: String,
+}
+
+#[derive(Deserialize)]
+pub struct OptimizedItem{
+    pub name: String,
+    pub cost: f64,
+    pub link: String
 }
 
 pub async fn gptcall(prompt: String) -> Result<String, Error>{
@@ -55,8 +64,28 @@ pub async fn gptcall(prompt: String) -> Result<String, Error>{
 }
 pub async fn shoppingapicall(item: String) -> Result<String, Error>{
     let client = reqwest::Client::new();
-    let response = client.get(format!("https://api.shoppingscraper.com/search/googleshopping/ca/?keyword={}&api_key={}&page=1&limit=10", item, keys::SHOPPING_SCRAPPER_KEY)).send().await?;
-    let response_body = response.text().await?;
-    println!("{:?}", response_body);
-    Ok(response_body)
+    let response: reqwest::Response = client.get(format!("https://api.shoppingscraper.com/search/googleshopping/ca/?keyword={}&api_key={}&page=1&limit=10", item, keys::SHOPPING_SCRAPPER_KEY)).send().await?;
+    let data: serde_json::Value = response.json().await?;
+    let item_prices = data.get("shoppingscraper").unwrap().get("results").unwrap();
+    println!("{:?}", item_prices);
+    println!("{}", type_of(&item_prices));
+    if let Some(prices_array) = item_prices.as_array() {
+        for item in prices_array {
+            // Now you can access individual JSON objects
+            if let Some(link_json) = item.get("link") {
+                if let Ok(link) = from_value::<String>(link_json.clone()) {
+                    println!("{}", link);
+                } else {
+                    println!("Failed to deserialize the name into a String");
+                }
+            }
+        }
+    }
+    // let response_body = response.text().await?;
+    //println!("{:?}", response_body);
+    // Ok(response_body)
+    Ok("LMAOOO".to_string())
+}
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
 }
