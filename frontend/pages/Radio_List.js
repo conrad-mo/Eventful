6faf44684e20a64e00810900a814f25edd0f4758
Verusins <115052147+Verusins@users.noreply.github.com>
2023-10-14 20:30:58 -0400

    Generate FINALLY WORKSSSSS!!!!! :):):)

--- frontend/pages/Radio_List.js
+++ frontend/pages/Radio_List.js
@@ -1,17 +1,18 @@
-import { useState,useEffect} from "react";
-import { ScrollView, StyleSheet, TouchableOpacity, View,Text,Modal,} from "react-native";
-import { Button, IconButton, TextInput,ActivityIndicator} from "react-native-paper";
+import { useState, useEffect } from "react";
+import { ScrollView, StyleSheet, TouchableOpacity, View, Text, Model } from "react-native";
+import { Button, IconButton, TextInput, ActivityIndicator } from "react-native-paper";
 import { CheckBox } from "react-native-elements";
 
 
 
-export default function RadioList({navigation, route}){
+export default function RadioList({ navigation }) {
+    // let it = ['ahhhh','b','c','d','eeeeeeeeeee','f','gggg','h','i','jjjjjjjj'];
     let [it, setIt] = useState([]);
-    const [checked,setChecked] = useState(new Array(15).fill(false));
+    const [checked, setChecked] = useState(new Array(15).fill(false));
     let [isLoading, setIsLoading] = useState(true);
     let [error, setError] = useState();
     let [response, setResponse] = useState();
-    const {eventName, searchQuery} = route.params;
+    const { eventName, searchQuery } = route.params;
     let [ChosenArray, setChosenArray] = useState([]);
 
 
@@ -28,9 +29,9 @@ export default function RadioList({navigation, route}){
                     "budget": +searchQuery
                 }),
             });
-          
+
             const data = await result.json()
-            
+
             setIt(data);
             setIsLoading(false);
             setResponse(data);
@@ -44,15 +45,15 @@ export default function RadioList({navigation, route}){
         getData();
     }, []);
 
-    const getContent = () =>{
+    const getContent = () => {
         if (isLoading) {
-            
+
             return (
                 <View>
-                  <ActivityIndicator size="large" animating={true} color='#EE4266' />
-                  <Text style={{ marginTop: 30, fontSize: 18}}>Generating your items...</Text>
+                    <ActivityIndicator size="large" animating={true} color='#EE4266' />
+                    <Text style={{ marginTop: 20 }}>Generating your items...</Text>
                 </View>
-              );
+            );
 
         }
 
@@ -62,191 +63,184 @@ export default function RadioList({navigation, route}){
 
         if (!response) {
             return <Text>Invalid response format</Text>;
-          }
+        }
 
         return null;
     }
-    
-    const handleOnChange = (id) =>{
+
+    const handleOnChange = (id) => {
         const updatedChecked = checked.map((item, index) =>
-        index === id ? !item : item
+            index === id ? !item : item
         );
         setChecked(updatedChecked);
     }
-    const getItems = (item) =>{
+    const getItems = (item) => {
         setChosenArray(ChosenArray => [...(ChosenArray || []), item]);
         console.log(ChosenArray);
     }
 
-    let items = it.map((item,index)=> 
-        index < 10?
-        <TouchableOpacity key = {item}>
-            <View >
-            <CheckBox
-            backgroundcolor = '#FDECF0'
-            center
-            title={item}
-            size={26}
-            right
-            checkedIcon='dot-circle-o'
-            uncheckedIcon='circle-o'
-            checkedColor="#EE4266"
-            checked={checked[it.indexOf(item)]}
-            containerStyle = {styles.itembox}
-            textStyle = {{fontSize: 20, fontWeight: 'normal'}}
-            onPress={() => {
-                ChosenArray.push(item);
-                getItems(item)
-                handleOnChange(it.indexOf(item))}} 
-                
-            />
-            
-            </View>
-        </TouchableOpacity> : ""
-    );
-
-   const [visibility,setVisibility] = useState(false);
-   const [text, setText] = useState("");
-    return (
-        <View   style={styles.loading}>
-             {getContent()}
-            {!isLoading && (
-                <View>
-            <IconButton style = {{alignSelf:'flex-end'}} size = {32} icon="plus" onPress = {()=>{
-                setVisibility(true);
-            }}/>
-            <Modal
-            animationType="slide" 
-            transparent = {true}
-            visible={visibility}
-            onDismiss={()=>{setVisibility(false)}}
-            onRequestClose={() => {
-             Alert.alert('Modal has been closed.');
-            setVisibility(false);
-        }}>
-         <View>
-            <TextInput
-                clearButtonMode="always"
-                mode="outlined"
-                style = {styles.centeredView}
-                label="add item"
-                value={text}
-                onChangeText={text => setText(text)}
-                activeOutlineColor="#EE4266"
-                />
-        </View>
-            <Button onPress={()=>{
-                if(items.length < 30){
-                    items.push(<TouchableOpacity>
-                        <View >
-                        <CheckBox
-                        backgroundcolor = '#FDECF0'
+    let items = it.map((item, index) =>
+        index < 10 ?
+            <TouchableOpacity key={item}>
+                <View >
+                    <CheckBox
+                        backgroundcolor='#FDECF0'
                         center
-                        title={text}
+                        title={item}
                         size={26}
                         right
                         checkedIcon='dot-circle-o'
                         uncheckedIcon='circle-o'
                         checkedColor="#EE4266"
-                        checked={checked[it.indexOf(text)]}
-                        containerStyle = {styles.itembox}
-                        textStyle = {fontSize = 28}
-                        onPress={() => handleOnChange(it.indexOf(text))}
-                        />
+                        checked={checked[it.indexOf(item)]}
+                        containerStyle={styles.itembox}
+                        textStyle={{ fontSize: 20, fontWeight: 'normal' }}
+                        onPress={() => handleOnChange(it.indexOf(item))}
+                    />
+                </View>
+            </TouchableOpacity> : ""
+    );
+
+    const [visibility, setVisibility] = useState(false);
+    const [text, setText] = useState("");
+    return (
+        <View style={styles.loading}>
+            {getContent()}
+            {!isLoading && (
+                <View>
+                    <IconButton style={{ alignSelf: 'flex-end' }} size={32} icon="plus" onPress={() => {
+                        setVisibility(true);
+                    }} />
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={visibility}
+                        onDismiss={() => { setVisibility(false) }}
+                        onRequestClose={() => {
+                            Alert.alert('Modal has been closed.');
+                            setVisibility(false);
+                        }}>
+                        <View>
+                            <TextInput
+                                clearButtonMode="always"
+                                mode="outlined"
+                                style={styles.centeredView}
+                                label="add item"
+                                value={text}
+                                onChangeText={text => setText(text)}
+                                activeOutlineColor="#EE4266" />
                         </View>
-                    </TouchableOpacity>)
-                    // console.log({items})
-                }
-                setVisibility(false)
-                }}> done!</Button>
-        </Modal>
-    
-        <View style = {{flexDirection:"column", width: 350, justifyContent :'center'}}>
-            <View style ={styles.textwrap}>
-                <View style = {{flexDirection:'row'}}>
-                <Text style = {{fontWeight: 600, fontSize : 28}} > Here's a </Text>
-                <Text style = {{fontWeight: 600, fontSize : 28,color :'#EE4266' }}> list of Items</Text>
-               </View>
-                <Text style = {{fontWeight: 600, fontSize : 28}}> we came up with </Text>
-             </View>
-            <View style = {{marginLeft:0,marginBottom:50,alignSelf:'center',height: 300, width:'110%'}}>
-                <ScrollView contentContainerStyle = {styles.container}>
-                    {items}
-                </ScrollView>
-            </View>
-        <Button labelStyle = {styles.buttontext} onPress={() => navigation.navigate('Generate', { chosenItems: ChosenArray })} style = {styles.button} mode = "contained">Optimize Cost!</Button>
-        </View>
-        </View>
+                        <Button onPress={() => {
+                            if (items.length < 10) {
+                                items = [...items, <TouchableOpacity>
+                                    <View >
+                                        <CheckBox
+                                            backgroundcolor='#FDECF0'
+                                            center
+                                            title={text}
+                                            size={26}
+                                            right
+                                            checkedIcon='dot-circle-o'
+                                            uncheckedIcon='circle-o'
+                                            checkedColor="#EE4266"
+                                            checked={checked[it.indexOf(text)]}
+                                            containerStyle={styles.itembox}
+                                            textStyle={fontSize = 28}
+                                            onPress={() => handleOnChange(it.indexOf(text))}
+                                        />
+                                    </View>
+                                </TouchableOpacity>]
+                            }
+                            setVisibility(false)
+                        }}> done!</Button>
+                    </Modal>
+
+                    <View style={{ flexDirection: "column", width: 350, justifyContent: 'center' }}>
+                        <View style={styles.textwrap}>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Text style={{ fontWeight: 600, fontSize: 28 }} > Here's a </Text>
+                                <Text style={{ fontWeight: 600, fontSize: 28, color: '#EE4266' }}> list of Items</Text>
+                            </View>
+                            <Text style={{ fontWeight: 600, fontSize: 28 }}> we came up with </Text>
+                        </View>
+                        <View style={{ marginLeft: 0, marginBottom: 50, alignSelf: 'center', height: 300, width: '110%' }}>
+                            <ScrollView contentContainerStyle={styles.container}>
+                                {items}
+                            </ScrollView>
+                        </View>
+                        <Button labelStyle={styles.buttontext} onPress={() => navigation.navigate('Generate')} style={styles.button} mode="contained">Optimize Cost!</Button>
+                    </View>
+                </View>
             )}
         </View>
     )
 
-    }
+}
 
 const styles = StyleSheet.create({
-    
-    loading:{
+
+    loading: {
         flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    },
-    container:{
-        padding:10,
-        backgroundcolor : '#FFFFFF',
-        flexDirection : 'column',
-        borderColor : '#303030',
+        alignItems: 'center',
         justifyContent: 'center',
-        
-        
     },
-    itembox :{
-        flexDirection : 'row',
-        backgroundColor :'rgba(238, 66, 102, 0.1)', 
-        padding: 5, 
+    container: {
+        padding: 10,
+        backgroundcolor: '#FFFFFF',
+        flexDirection: 'column',
+        borderColor: '#303030',
+        justifyContent: 'center',
+
+
+    },
+    itembox: {
+        flexDirection: 'row',
+        backgroundColor: 'rgba(238, 66, 102, 0.1)',
+        padding: 5,
         paddingLeft: 20,
         height: 60,
         borderRadius: 2,
     },
-    itemtext:{
-        borderRadius:6,
-        padding : 5,
-        textAlign : 'right',
-        
+    itemtext: {
+        borderRadius: 6,
+        padding: 5,
+        textAlign: 'right',
+
     },
-    textwrap : {
-        justifyContent:'center',
-        marginTop : 50,
-        marginBottom : 50,
-        marginLeft : 45
-        
+    textwrap: {
+        justifyContent: 'center',
+        marginTop: 50,
+        marginBottom: 50,
+        marginLeft: 45
+
     },
-    button : {
-        marginBottom : 50,
-        marginTop : 50,
+    button: {
+        marginBottom: 50,
+        marginTop: 50,
         // marginLeft : 45,
-        height : 40,
+        height: 40,
         width: '90%',
         left: '5%',
-        backgroundColor : '#303030',
-        justifyContent :'center',
+        backgroundColor: '#303030',
+        justifyContent: 'center',
         borderRadius: 10
     },
-    buttontext :{
-        justifyContent : 'center',
-        alignItems : 'center'
+    buttontext: {
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    add:{
-        alignSelf:'flex-end'
+    add: {
+        alignSelf: 'flex-end'
     },
-    centeredView:{
+    centeredView: {
         marginTop: '90%',
-        justifyContent:'center',
-        alignSelf:'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
         height: 60,
-        width:'80%',
-        backgroundColor:'#FFFFFF',
-        borderRadius:10,
-        fontSize : 24
+        width: '80%',
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        fontSize: 24
 
     }
 }
